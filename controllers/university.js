5cf4b72447dc81d8fe8eb2e6fbe1fa7b27a325b7
niharika6218 <123114147+niharika6218@users.noreply.github.com>
2023-04-14 11:35:50 -0500

    comiited

--- controllers/university.js
+++ controllers/university.js
@@ -12,9 +12,17 @@ exports.university_list = async function(req, res) {
     }
    };
 // for a specific university.
-exports.university_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: university detail: ' + req.params.id);
-};
+// for a specific university.
+exports.university_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await university.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
 // Handle university create on POST.
 exports.university_create_post = async function(req, res) {
     console.log(req.body)
@@ -22,7 +30,7 @@ exports.university_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"university_type":"goat", "cost":12, "size":"large"}
+    // {"universityname":"goat", "cost":12, "size":"large"}
     document.universityname = req.body.universityname;
     document.departmentname = req.body.departmentname;
     document.score = req.body.score;
@@ -40,8 +48,25 @@ exports.university_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: university delete DELETE ' + req.params.id);
 };
 // Handle university update form on PUT.
-exports.university_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: university update PUT' + req.params.id);
+// Handle university update form on PUT.
+exports.university_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await university.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.universityname)
+ toUpdate.universityname = req.body.universityname;
+ if(req.body.departmentname) toUpdate.cost = req.body.departmentname;
+ if(req.body.score) toUpdate.size = req.body.score;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+ }
 };
 
 // VIEWS
